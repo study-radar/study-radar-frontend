201cf39b6364fbaa5c2e94c6e51d8e194d18843d
yolanda10202 <yolanda10202@gmail.com>
2021-11-20 13:43:22 -0800

    moved calendar in calendar link to home page and deleted calendar link.

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,12 +1,9 @@
 
 import * as React from 'react';
-// import React, { useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
-import { Link, useNavigate, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import * as ReactDOM from 'react-dom';
 import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
-
+import "./calendar.css";
 
 
 class Calendar extends React.Component {
@@ -15,8 +12,8 @@ class Calendar extends React.Component {
         this.data = [{
                 Id: 2,
                 Subject: 'Meeting',
-                StartTime: new Date(2018, 1, 15, 10, 0),
-                EndTime: new Date(2018, 1, 15, 12, 30),
+                StartTime: new Date(new Date().setHours(new Date().getHours() - 3)),
+                EndTime: new Date(new Date().setHours(new Date().getHours() + 3)),
                 IsAllDay: false,
                 Status: 'Completed',
                 Priority: 'High'
@@ -24,36 +21,33 @@ class Calendar extends React.Component {
             document.getElementById('schedule');
     }
 
-    // navigate = useNavigate();
-    // handleGoBack() {
-    //     navigate(-1);
-    // }
-
     render() {
-        return <div><ScheduleComponent height='550px' selectedDate={new Date(2018, 1, 15)} eventSettings={{ dataSource: this.data,
-            fields: {
-                id: 'Id', 
-                subject: { name: 'Subject' },
-                isAllDay: { name: 'IsAllDay' },
-                startTime: { name: 'StartTime' },
-                endTime: { name: 'EndTime' }
-            }
-        }}>
-      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
-    </ScheduleComponent>
-    <br />
-    <button 
-            className = "buttonProp"
-            onclick = "useNavigate(-1)"
-            // history.go(-1), history.back() --> doesn't work
-            type="submit"
-            
-            // onclick = "/"
-            // disabled={loading}
-            >BACK TO HOMEPAGE
-    </button>
-    </div>;
-    
+        return <div className="home">
+            <header>
+                <div className="navbar">
+                {/* <button onClick={handleLogout} className="logout">Log Out</button> */}
+                <Link to="/update-profile">Update Profile</Link> 
+                </div>
+            </header>
+            <div className="container">
+                <div className="feed">
+                    FEED
+                </div>
+                <div class="calendar">
+                    <ScheduleComponent height="650px" selectedDate={new Date(new Date().setHours(new Date().getHours() - 3))} eventSettings={{ dataSource: this.data,
+                        fields: {
+                            id: 'Id', 
+                            subject: { name: 'Subject' },
+                            isAllDay: { name: 'IsAllDay' },
+                            startTime: { name: 'StartTime' },
+                            endTime: { name: 'EndTime' }
+                        }
+                    }}>
+                    <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+                    </ScheduleComponent>
+                </div>
+            </div>
+        </div>;
     }
 };
 
