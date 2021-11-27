a37e123699a5d965fcb54588390d9c7b790e576f
AlwaysVeryHappy <gabrielx1973@gmail.com>
2021-11-27 12:23:51 -0800

    add browse weeks capability

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import { useUserGroup } from "../contexts/UserGroupContext";
 import AgendaItem from "./AgendaItem";
 
 export default function Agenda() {
     const {userGroups} = useUserGroup();
 
-    function logGroupsForUser() {
-        console.log(userGroups);
+    // The months start at 0
+    const [currentWeek, setCurrentWeek] = useState('2021-10-28');
+
+    React.useEffect(() => {
+        getCurrentWeek();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+
+    function getCurrentWeek(){
+        const today = new Date();
+        const todayDay = today.getDay();
+        let firstDayOfWeek = new Date();
+        firstDayOfWeek.setDate(today.getDate() - todayDay);
+        setCurrentWeek(`${firstDayOfWeek.getFullYear()}-${firstDayOfWeek.getMonth()}-${firstDayOfWeek.getDate()}`);
+    }
+
+    function incrementWeek() {
+        setCurrentWeek((prevWeek) => {
+            const nextFirstDayOfWeek = new Date();
+            const currentWeekSplit = prevWeek.split('-');
+            nextFirstDayOfWeek.setFullYear(currentWeekSplit[0]);
+            nextFirstDayOfWeek.setMonth(currentWeekSplit[1]);
+            nextFirstDayOfWeek.setDate(currentWeekSplit[2]);
+            // Increment the date by 7 for next week
+            nextFirstDayOfWeek.setDate(nextFirstDayOfWeek.getDate() + 7);
+            return `${nextFirstDayOfWeek.getFullYear()}-${nextFirstDayOfWeek.getMonth()}-${nextFirstDayOfWeek.getDate()}`;
+        })
+    }
+
+    function decrementWeek() {
+        setCurrentWeek((prevWeek) => {
+            const nextFirstDayOfWeek = new Date();
+            const currentWeekSplit = prevWeek.split('-');
+            nextFirstDayOfWeek.setFullYear(currentWeekSplit[0]);
+            nextFirstDayOfWeek.setMonth(currentWeekSplit[1]);
+            nextFirstDayOfWeek.setDate(currentWeekSplit[2]);
+            // Decrement the date by 7 for next week
+            nextFirstDayOfWeek.setDate(nextFirstDayOfWeek.getDate() - 7);
+            return `${nextFirstDayOfWeek.getFullYear()}-${nextFirstDayOfWeek.getMonth()}-${nextFirstDayOfWeek.getDate()}`;
+        })
     }
 
     return (
     <>
+    <p>{currentWeek}</p>
+    <br/>
+    <button onClick={incrementWeek}>Next Week</button>
+    <br/>
+    <button onClick={decrementWeek}>Last Week</button>
+    <br/>
     {(userGroups).map((group) => {
         return (
           <AgendaItem
