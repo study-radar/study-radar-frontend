6cb470519761322dc2855aa311b55fe11476e8a9
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2021-11-27 19:10:39 -0800

    Padded the minutes in event times with 0's and reversed the sorting

--- src/components/AgendaItemList.js
+++ src/components/AgendaItemList.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React from "react";
 import { useUserGroup } from "../contexts/UserGroupContext";
 import AgendaItem from "./AgendaItem";
 
 export default function AgendaItemList(props) {
-    const {userGroups} = useUserGroup();
+  const { userGroups } = useUserGroup();
 
-    // Props should include the dateString to render
-    return (
-        <>
-        {userGroups.filter((group) => {
-            // Convert group date time
-            const groupDateTimeStringSplit = group.date_time.split('T');
-            const groupDateString = groupDateTimeStringSplit[0];
-            // Covert to months that start with 0
-            const groupDateStringSplit = groupDateString.split('-');
-            groupDateStringSplit[1] = (parseInt(groupDateStringSplit[1]) - 1).toString();
-            const convertedGroupDateString = groupDateStringSplit.join('-');
-            return props.dateString === convertedGroupDateString;
-        }).sort(function (firstGroup, secondGroup) {
-            const firstGroupTime = firstGroup.date_time.split('T')[1];
-            const secondGroupTime = secondGroup.date_time.split('T')[1];
-            if (firstGroupTime > secondGroupTime) {
-                return -1;
-            }
-            if (firstGroupTime < secondGroupTime) {
-                return 1;
-            }
-            return 0;
-        }).map((group) => <AgendaItem
+  // Props should include the dateString to render
+  return (
+    <>
+      {userGroups
+        .filter((group) => {
+          // Convert group date time
+          const groupDateTimeStringSplit = group.date_time.split("T");
+          const groupDateString = groupDateTimeStringSplit[0];
+          // Covert to months that start with 0
+          const groupDateStringSplit = groupDateString.split("-");
+          groupDateStringSplit[1] = (
+            parseInt(groupDateStringSplit[1]) - 1
+          ).toString();
+          const convertedGroupDateString = groupDateStringSplit.join("-");
+          return props.dateString === convertedGroupDateString;
+        })
+        .sort(function (firstGroup, secondGroup) {
+          const firstGroupTime = firstGroup.date_time.split("T")[1];
+          const secondGroupTime = secondGroup.date_time.split("T")[1];
+          if (firstGroupTime > secondGroupTime) {
+            return 1;
+          }
+          if (firstGroupTime < secondGroupTime) {
+            return -1;
+          }
+          return 0;
+        })
+        .map((group) => (
+          <AgendaItem
             key={group.id}
             date_time={group.date_time}
             id={group.id}
             name={group.name}
-        />)
-        }
-        </>
-    );
+          />
+        ))}
+    </>
+  );
 }
