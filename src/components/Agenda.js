e4a5187b68550f929e409888c8bec154d6476b48
AlwaysVeryHappy <gabrielx1973@gmail.com>
2021-11-27 17:53:55 -0800

    can get all days in current week

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { useUserGroup } from "../contexts/UserGroupContext";
 import AgendaItem from "./AgendaItem";
 
@@ -6,23 +6,31 @@ export default function Agenda() {
     const {userGroups} = useUserGroup();
 
     // The months start at 0
-    const [currentWeek, setCurrentWeek] = useState('2021-10-28');
+    const [currentWeekStart, setCurrentWeekStart] = useState('2021-10-28');
+    // List of strings for the week
+    const [currentWeek, setCurrentWeek] = useState([]);
 
-    React.useEffect(() => {
-        getCurrentWeek();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+    useEffect(() => {
+      getCurrentWeek();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    // Makes sure to refresh the week everytime the currentWeekStart changes
+    useEffect(() => {
+      createWeek(currentWeekStart);
+    }, [currentWeekStart]);
 
     function getCurrentWeek(){
         const today = new Date();
         const todayDay = today.getDay();
         let firstDayOfWeek = new Date();
         firstDayOfWeek.setDate(today.getDate() - todayDay);
-        setCurrentWeek(`${firstDayOfWeek.getFullYear()}-${firstDayOfWeek.getMonth()}-${firstDayOfWeek.getDate()}`);
+        setCurrentWeekStart(`${firstDayOfWeek.getFullYear()}-${firstDayOfWeek.getMonth()}-${firstDayOfWeek.getDate()}`);
+        // createWeek(currentWeekStart);
     }
 
     function incrementWeek() {
-        setCurrentWeek((prevWeek) => {
+      setCurrentWeekStart((prevWeek) => {
             const nextFirstDayOfWeek = new Date();
             const currentWeekSplit = prevWeek.split('-');
             nextFirstDayOfWeek.setFullYear(currentWeekSplit[0]);
@@ -31,11 +39,12 @@ export default function Agenda() {
             // Increment the date by 7 for next week
             nextFirstDayOfWeek.setDate(nextFirstDayOfWeek.getDate() + 7);
             return `${nextFirstDayOfWeek.getFullYear()}-${nextFirstDayOfWeek.getMonth()}-${nextFirstDayOfWeek.getDate()}`;
-        })
+        });
+        // createWeek(currentWeekStart);
     }
 
     function decrementWeek() {
-        setCurrentWeek((prevWeek) => {
+      setCurrentWeekStart((prevWeek) => {
             const nextFirstDayOfWeek = new Date();
             const currentWeekSplit = prevWeek.split('-');
             nextFirstDayOfWeek.setFullYear(currentWeekSplit[0]);
@@ -44,12 +53,44 @@ export default function Agenda() {
             // Decrement the date by 7 for next week
             nextFirstDayOfWeek.setDate(nextFirstDayOfWeek.getDate() - 7);
             return `${nextFirstDayOfWeek.getFullYear()}-${nextFirstDayOfWeek.getMonth()}-${nextFirstDayOfWeek.getDate()}`;
-        })
+        });
+      // createWeek(currentWeekStart);
+    }
+
+    function createWeek(weekStart) {
+      // Create a Date object for current week start
+      const currentWeekStartDate = new Date();
+      const currentWeekSplit = weekStart.split('-');
+      console.log(currentWeekSplit);
+      currentWeekStartDate.setFullYear(currentWeekSplit[0]);
+      currentWeekStartDate.setMonth(currentWeekSplit[1]);
+      currentWeekStartDate.setDate(currentWeekSplit[2]);
+      // Go through and create strings for each day in this week
+      const weekDayStrings = [];
+      for (let i = 0; i < 7; i++){
+        // Note: Need to set the day to today, then set date
+        const dayOfWeek = new Date();
+        dayOfWeek.setFullYear(currentWeekSplit[0]);
+        dayOfWeek.setMonth(currentWeekSplit[1]);
+        dayOfWeek.setDate(currentWeekSplit[2]);
+        dayOfWeek.setDate(currentWeekStartDate.getDate() + i);
+        weekDayStrings.push(`${dayOfWeek.getFullYear()}-${dayOfWeek.getMonth()}-${dayOfWeek.getDate()}`);
+      }
+      setCurrentWeek(weekDayStrings);
     }
 
     return (
     <>
-    <p>{currentWeek}</p>
+    <p>{currentWeekStart}</p>
+    <br/>
+    {(currentWeek).map((dateString) => {
+        return (
+          <>
+          <p>{dateString}</p>
+          <br/>
+          </>
+        );
+      })}
     <br/>
     <button onClick={incrementWeek}>Next Week</button>
     <br/>
